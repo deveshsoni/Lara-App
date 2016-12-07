7528c6da87f6f74b6b17fd0234399e558c43009b
deveshsoni <erdeveshsoni@gmail.com>
2016-12-08 02:11:05 +0530

    Update profile.blade.php

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -9,7 +9,7 @@ Introduction
 @stop
 
 @section('description')
-<p>Hello everyone ! This is <strong>Devesh</strong>. Born in Jodhpur,Rajasthan. Completed my schooling anf graducation from Jodhpur. Put up in Bangalore for past 2 months.</p>
+<p>Hello everyone ! This is <strong>Devesh</strong>. Born in Jodhpur,Rajasthan. Completed my schooling and graduation from Jodhpur. Put up in Bangalore for past 2 months.</p>
 @stop
 
 @section('title2')
